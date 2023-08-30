620f2ce7915a6bdb3b56b1e21b3fe97fa4ba1f0a
Matt Hrono <37225540+mhrono@users.noreply.github.com>
2023-08-30 13:39:32 -0400

    Update PkgInfoReader.py

--- CommonProcessors/PkgInfoReader.py
+++ CommonProcessors/PkgInfoReader.py
@@ -584,6 +584,8 @@ class PkgInfoReader(Copier):
             self.env["infodict"] = cataloginfo
             self.env["version"] = cataloginfo["version"]
             self.env["minimum_os_version"] = cataloginfo["minimum_os_version"]
+            self.env["installer_item_size"] = cataloginfo["installer_item_size"]
+            self.env["installed_size"] = cataloginfo["installed_size"]
 
         finally:
             if dmg:
