d449094fe9982ad3b98a7e0cc0ccd9ce67aac1a7
Matt Hrono <37225540+mhrono@users.noreply.github.com>
2023-09-26 13:04:12 -0400

    Update PkgInfoReader.py

--- CommonProcessors/PkgInfoReader.py
+++ CommonProcessors/PkgInfoReader.py
@@ -513,9 +513,9 @@ class PkgInfoReader(Copier):
         for infoitem in receiptinfo:
             if APLooseVersion(infoitem["version"]) > APLooseVersion(highestpkgversion):
                 highestpkgversion = infoitem["version"]
-            if "installKBytes" in infoitem:
+            if "installed_size" in infoitem:
                 # note this is in KBytes
-                installedsize += infoitem["installKBytes"]
+                installedsize += infoitem["installed_size"]
 
         if metaversion == "0.0.0.0.0":
             metaversion = highestpkgversion
